98be3fb054f1bbefa8791c990cd33bf10f93d840
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-07-26 13:32:50 +0100

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { ROUTES } from 'utils/constants';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -53,44 +43,6 @@ export default function SignInSide() {
 		<Grid container component='main' className={classes.root}>
 			<CssBaseline />
 			<Grid item xs={false} sm={4} md={7} className={classes.image} />
-			<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-				<div className={classes.paper}>
-					<Avatar className={classes.avatar}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component='h1' variant='h5'>
-						Check in
-					</Typography>
-					<form className={classes.form} noValidate>
-						<TextField
-							variant='outlined'
-							margin='normal'
-							required
-							fullWidth
-							id='text'
-							label='Staff ID'
-							name='staffID'
-							autoComplete='text'
-							autoFocus
-						/>
-
-						<FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
-
-						<Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
-							Check In
-						</Button>
-                        
-						<Grid container>
-							<Grid item xs>
-								Not yet a staff?
-								<Link href={ROUTES.CREATE_STAFF} className={classes.link} variant='body2'>
-									Become one now
-								</Link>
-							</Grid>
-						</Grid>
-					</form>
-				</div>
-			</Grid>
 		</Grid>
 	);
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ROUTES = {
-	HOME: '/',
-	LOGIN: '/login',
+	HOME: '/staff',
+	LOGIN: '/',
 	CREATE_STAFF: '/create-staff',
 };
 
